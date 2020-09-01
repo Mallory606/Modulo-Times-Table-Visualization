d5fd369d7c79f98931ddb562490546d00ac6e80d
Ashley K <buginla@gmail.com>
2020-08-31 18:44:29 -0600

    Initialized some new classes.

---
+++ src/Display.java
@@ -0,0 +1,11 @@
+import javafx.stage.Stage;
+
+public class Display extends javafx.application.Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public void startWindow(String[] args){ launch(args); }
+}
--- src/ModuloTimesTableVisualization.java
+++ src/ModuloTimesTableVisualization.java
@@ -1,5 +1,6 @@
 public class ModuloTimesTableVisualization{
     public static void main(String[] args){
-
+        Display window = new Display();
+        window.startWindow(args);
     }
 }
